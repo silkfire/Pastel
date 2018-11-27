e60550c1c97ab0349ad48e75b48a03a18c602c02
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2018-11-27 18:54:57 +0100

    Initial commit

---
+++ src/ConsoleExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace Pastel
+{
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+    using System.Runtime.InteropServices;
+
+
+    public static class ConsoleExtensions
+    {
+
+        static ConsoleExtensions()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var iStdOut =   GetStdHandle(STD_OUTPUT_HANDLE);
+
+                var enable  =   GetConsoleMode(iStdOut, out var outConsoleMode)
+                             && SetConsoleMode(iStdOut, outConsoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN);
+            }
+        }
+
+
+        private const int  STD_OUTPUT_HANDLE                     = -11;
+        private const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+        private const uint DISABLE_NEWLINE_AUTO_RETURN        = 0x0008;
+
+        [DllImport("kernel32.dll")]
+        private static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint GetLastError();
+
+
+
+
+        /// <summary>
+        /// Returns a string wrapped in an ANSI color code using the specified color.
+        /// </summary>
+        /// <param name="input">The string to color.</param>
+        /// <param name="color">The color to use on the specified string.</param>
+        public static string Pastel(this string input, Color color)
+        {
+            return $"\u001b[38;2;{color.R};{color.G};{color.B}m{input}\u001b[0m";
+        }
+
+        /// <summary>
+        /// Returns a string wrapped in an ANSI color code using the specified color.
+        /// </summary>
+        /// <param name="input">The string to color.</param>
+        /// <param name="hexColor">The color to use on the specified string.<para>Supported format: [#]RRGGBB.</para></param>
+        public static string Pastel(this string input, string hexColor)
+        {
+            var color = Color.FromArgb(int.Parse(hexColor.Replace("#", ""), NumberStyles.HexNumber));
+
+            return Pastel(input, color);
+        }
+    }
+}
